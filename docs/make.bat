088fc11e26ef7851ed13f2a6b8076a2dbc87aa1e
Jan Van Bruggen <jancvanbruggen@gmail.com>
2014-10-07 00:19:52 -0700

    Experimenting... The time is near...

--- docs/make.bat
+++ docs/make.bat
@@ -68,9 +68,12 @@ if "%1" == "gh-pages" (
 	call git checkout master %GH_PAGES_SOURCES%
 	call git reset HEAD
 	call make html
-	move .\build\html\*.html ..
+	move .\build\html\*.* ..
+    move .\build\html\_sources ..
+    move .\build\html\_static ..
 	rmdir /s /q %GH_PAGES_SOURCES% build
 	call git add -A
+    call git reset .
 	call git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`" && git push origin gh-pages ; git checkout master
 	echo.Build finished. The GitHub Pages Documentation site is updated.
 	goto end
