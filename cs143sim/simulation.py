68469f9e3303c5637f39c42917785c854a38909b
fictivelogic <sricherd@gmail.com>
2014-11-01 22:09:09 -0700

    Cleaned up input file parsing code.
    I made my comments nicer-looking, and I added some TODOs.

--- cs143sim/simulation.py
+++ cs143sim/simulation.py
@@ -43,20 +43,20 @@ class InputFileSyntaxError(Exception):
         self.message = message
 
     def __str__(self):
-        return 'Input File Syntax Error: (Line ' + self.line_number + ') ' + self.message
+        return 'Input File Syntax Error: (Line ' + str(self.line_number) + ') ' + self.message
 
 
 class InputFileUnknownReference(Exception):
     """
     InputFileUnknownReference is an `Exception` thrown when a link or host makes reference
-    to an unknown link or object
+    to an unknown object (Host/Router/Link)
     """
     def __init__(self, line_number, message):
         self.line_number = line_number
         self.message = message
 
     def __str__(self):
-        return 'InputFileUnknownReference (Line ' + self.line_number + '): ' + self.message
+        return 'InputFileUnknownReference (Line ' + str(self.line_number) + '): ' + self.message
 
 
 class Controller:
@@ -120,7 +120,7 @@ class Controller:
             elif isinstance(actor, Router):
                 actor.links.append(new_link)
             else:
-                pass  # raise Exception('Unknown Source/Destination: ' + actor)
+                raise Exception('Unknown Source/Destination: ' + actor)
         self.links[name] = new_link
 
     def make_router(self, name, ip_address):
@@ -138,20 +138,24 @@ class Controller:
         :param str case: path to simulation input file
         """
         with open(case, 'rb') as case_file:
-            # Open the file for line-by-line consumption (NOM NOM NOM)
-            obj_type = ''  # make an empty object, it will contain the pointer to the
-            obj_id = ''
+            # Open the file for line-by-line consumption
+            obj_type = ''  # obj_type holds the current object type (LINK/HOST/Etc) to which attributes apply
+            obj_id = ''    # obj_id is the current ID of the object
+            # These are "simple" attributes that have only 1 argument.
+            # Not included in this list is the CONNECTS attribute, which has 2 arguments,
+            #   and ID, which requires special processing.
             attributes = ('RATE', 'DELAY', 'DATA', 'BUFFER', 'DST', 'SRC', 'START', 'IP')
-            store_in = {attribute: '' for attribute in attributes}
+            store_in = {attribute: '' for attribute in attributes}  # initialize all attributes to ''
             line_number = 0
             for case_line in case_file:
                 line_number += 1
                 line_comp = case_line.split()
                 if line_comp == [] and obj_id == '':
-                    obj_id = ''  # clear obj_ID on extra empty line
-                    obj_type = ''  # clear obj_type
+                    obj_id = ''  # clear obj_ID and type on empty line
+                    obj_type = ''
                     continue
                 try:
+                    # if the line is empty, just set keyword to ''
                     keyword = line_comp[0].upper()
                 except AttributeError:
                     keyword = ''
@@ -165,7 +169,7 @@ class Controller:
                     obj_type = keyword
                     obj_id = ''
                 elif keyword in attributes:
-                    # store simple attributes in their corresponding place
+                    # store simple attributes in their place in the store_in dictionary
                     store_in[keyword] = line_comp[1]
 
                 elif keyword == 'ID' or (keyword == '' and obj_id != ''):
@@ -176,8 +180,8 @@ class Controller:
                     if obj_id == '':
                         obj_id = line_comp[1].upper()
                     elif obj_type == 'LINK':
-                        # call the create function for the old object
-                        # but first, make sure all the variables have been declared!
+                        # if we're getting an additional ID attribute on a LINK
+                        # make sure we have all the attributes available, then create the link object
                         for attribute in ['BUFFER', 'DELAY', 'RATE', 'SRC', 'DST']:
                             if store_in[attribute] in ['', []]:
                                 # Make sure all the attributes are not empty
@@ -185,10 +189,10 @@ class Controller:
                         # If all the attributes are present, create the object
                         if DEBUG:
                             print 'Making Link: ' + obj_id
-                        the_src = ''
+                        the_src = ''  # temporary variables that will point to the actual src/dst instances
                         the_dst = ''
-                        # This next part verifies referential integrity (aka that the specified hosts/routers
-                        # actually exist in the simulation)
+                        # Enforce referential integrity (aka check that the specified
+                        # hosts/routers actually exist in the simulation)
                         for target in [store_in['SRC'], store_in['DST']]:
                             print 'Checking: ' + target
                             if target in self.hosts:
@@ -206,7 +210,7 @@ class Controller:
                         self.make_link(obj_id, the_src, the_dst, float(store_in['RATE']),
                                        float(store_in['DELAY']), int(store_in['BUFFER']))
                     elif obj_type == 'HOST':
-                        # check the attributes,
+                        # check the attribute(s) (there's only one for HOSTS so far: IP)
                         for attribute in ['IP']:
                             if store_in[attribute] in ['', []]:
                                 # Make sure all the attributes are not empty
@@ -216,6 +220,8 @@ class Controller:
                         self.make_host(obj_id, store_in['IP'])
 
                     elif obj_type == 'ROUTER':
+                        # check the attribute(s) (only one so far: IP)
+                        # TODO: Add router update-routing-tables value to attributes
                         for attribute in ['IP']:
                             if store_in[attribute] in ['', []]:
                                 raise MissingAttribute(obj_type, obj_id, attribute)
@@ -224,6 +230,7 @@ class Controller:
                         self.make_router(obj_id, store_in['IP'])
 
                     elif obj_type == 'FLOW':
+                        # TODO: Specify congestion control algorithm as attribute
                         for attribute in ['SRC', 'DST', 'START', 'DATA']:
                             if store_in[attribute] in ['', []]:
                                 raise MissingAttribute(obj_type, obj_id, attribute)
@@ -231,6 +238,8 @@ class Controller:
                         # BUT FIRST, we need to make sure the SRC/DST hosts actually exist..
                         # if they don't, warn the user that "No, i'm sorry, you have to specify
                         # hosts that actually exist."
+                        if DEBUG:
+                            print 'Making Flow: ' + obj_id
                         try:
                             self.make_flow(obj_id, self.hosts[store_in['SRC']], self.hosts[store_in['DST']],
                                            int(store_in['DATA']), float(store_in['START']))
@@ -239,10 +248,9 @@ class Controller:
                                                             'Input File Formatting Error: ' +
                                                             'Reference to unknown object: ' + repr(e))
 
-                        print 'Making Flow: ' + obj_id
                     else:
-                        # Why is there an ID tag?!??
-                        raise Exception('Input File formatting error.. Why is there an ID tag here?!')
+                        # Unexpected ID attribute (out of context of an object Type)
+                        raise InputFileSyntaxError(line_number, 'Unexpected "ID" attribute.')
                     if keyword == 'ID':
                         obj_id = line_comp[1].upper()
                     else:
@@ -253,10 +261,11 @@ class Controller:
                         store_in['SRC'] = line_comp[1].upper()
                         store_in['DST'] = line_comp[2].upper()
                     else:
-                        raise Exception('Input File Formatting Error: CONNECTS attribute formatted incorrectly.' +
-                                        '\nExpects: CONNECTS A B')
+                        raise InputFileSyntaxError(line_number,
+                                                   'Input File Formatting Error: CONNECTS attribute ' +
+                                                   'formatted incorrectly.\nExpects: CONNECTS A B')
                 else:
-                    raise Exception('Unrecognized keyword: ' + keyword)
+                    raise InputFileSyntaxError(line_number, 'Unrecognized keyword: ' + keyword)
 
     def run(self, until=None):
         """Run the simulation for a specified duration
